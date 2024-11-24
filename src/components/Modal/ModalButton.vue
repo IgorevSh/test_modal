acd677e1298e12db01ea34463025d1a313d4f51e
Danila <shumd100@gmail.com>
2024-11-24 13:43:41 +0400

    test modal

--- src/components/Modal/ModalButton.vue
+++ src/components/Modal/ModalButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button style="cursor: pointer" @click="showModal = true">Открыть</button>
 
-  <modal-window @select="customEmit" v-model="showModal" :title="'test'" />
+  <modal-window @select="customEmit" v-model="showModal" :title="modalTitle" />
 </template>
 
 <script setup lang="ts">
@@ -9,6 +9,7 @@ import ModalWindow from "@/components/Modal/ModalWindow.vue";
 import { ref } from "vue";
 
 const showModal = ref(true);
+const modalTitle = ref("test");
 function customEmit(id: string): void {
   alert(`Выбранный id ${id}`);
 }
